a6f6083a091b70e3e5c8f2482bfa1bebc536dbc7
bipinyadav3175 <bipinyadav3175@gmail.com>
2022-03-21 15:19:45 +0530

    fixed Actions.js path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const http = require("http")
 const {Server} = require("socket.io")
-const ACTIONS = require("./src/ACTIONS")
+const ACTIONS = require("./Actions")
 const path = require("path")
 
 const server = http.createServer(app)
